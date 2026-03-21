4438a3828c42cccbc4653a0838717a880d248184
30Eagle <gontusakethreddy@gmail.com>
2026-03-21 17:17:30 +0530

    Used properties file to send url and which browser to use, rather than hardcoding in BaseTest.

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,8 +1,14 @@
 package base;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -10,13 +16,33 @@ import org.testng.annotations.BeforeClass;
 public class BaseTest {
     public WebDriver driver;
 
+    
     @BeforeClass
-    public void setup() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("https://www.snapdeal.com/");
+    public void setup() throws IOException {
+    	
+    		Properties prop = new Properties();
+    		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"\\src\\test\\resources\\Config\\config.properties");
+    		prop.load(fis);
+    		String browserName = prop.getProperty("browser");
+    		System.out.println(browserName);
+    		if(browserName.equals("chrome"))
+    		{
+    			WebDriverManager.chromedriver().setup();
+    			driver = new ChromeDriver(); 
+    		}
+    		else if(browserName.equals("edge"))
+    		{
+    			WebDriverManager.edgedriver().setup();
+    			driver = new EdgeDriver();
+    		}
+    		String url = prop.getProperty("url");
+    		System.out.println(url);
+    		driver.get(url);
+
+    		driver.manage().window().maximize();
+	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        
+        
     }
 
 //    @AfterClass
